45fdbf667e12f6685cbc2473e69afaae368264bc
Evelyne Ngene <132668454+Evelynewaters@users.noreply.github.com>
2026-01-14 07:23:31 +0100

    config.py to include constants and remove code
    Removed extensive data processing and modeling code. Added constants for file paths and test size.

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+TRAIN_PATH = "E:/Data Sets/Loan Prediction/train_u6lujuX_CVtuZ9i.csv"
+TEST_PATH = "E:/Data Sets/Loan Prediction/test_Y3wMUE5_7gLdaTN.csv"
+OUTPUT_PATH = "Credit_Predictions.csv"
+TEST_SIZE = 0.2
